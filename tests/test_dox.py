ad3bbc6ab0982fb1a5138b75f874e833f58b0edf
cundo <peter.cunderlik@gmail.com>
2016-09-30 21:24:32 +0100

    Added code, other and root dir tests

--- tests/test_dox.py
+++ tests/test_dox.py
@@ -8,7 +8,7 @@ DOCKER_IMAGE = os.getenv("DOCKER_IMAGE", "realcundo/dtox:test")
 def run(*dtox_args, **kwargs):
     """Runs docker run with optional dtox_args. Returns ProcResult"""
 
-    env = STE("./test-output")
+    env = STE("./tests-workdir")
 
     command = ["docker", "run", "--rm", "-i", DOCKER_IMAGE]
     command.extend(dtox_args)
@@ -22,21 +22,49 @@ def run(*dtox_args, **kwargs):
     return r
 
 
-def test_dtox_empty_params_no_toxini_file():
+class TestNoToxIni:
 
-    r = run(expect_error=True)
+    def test_dtox_empty_params(self):
 
-    assert r.returncode != 0
-    assert r.stdout == "using CODE_DIR=/code\n"
-    assert r.stderr == "ERROR: toxini file 'tox.ini' not found\n"
-    assert r.files_after == {}
+        r = run(expect_error=True)
 
+        assert r.returncode != 0
+        assert r.stdout == "using CODE_DIR=/code\n"
+        assert r.stderr == "ERROR: toxini file 'tox.ini' not found\n"
+        assert r.files_after == {}
 
-def test_dtox_dot_dir_param_no_toxini_file():
+    def test_dtox_dot_dir_param(self):
 
-    r = run(".", expect_error=True)
+        r = run(".", expect_error=True)
 
-    assert r.returncode != 0
-    assert r.stdout == ""
-    assert r.stderr == "Error: Working directory must be absolute: .\n"
-    assert r.files_after == {}
+        assert r.returncode != 0
+        assert r.stdout == ""
+        assert r.stderr == "Error: Working directory must be absolute: .\n"
+        assert r.files_after == {}
+
+    def test_dtox_code_dir_param(self):
+
+        r = run("/code", expect_error=True)
+
+        assert r.returncode != 0
+        assert r.stdout == "using CODE_DIR=/code\n"
+        assert r.stderr == "ERROR: toxini file 'tox.ini' not found\n"
+        assert r.files_after == {}
+
+    def test_dtox_other_dir_param(self):
+
+        r = run("/this/dir/does/not/exist/yet", expect_error=True)
+
+        assert r.returncode != 0
+        assert r.stdout == "using CODE_DIR=/this/dir/does/not/exist/yet\n"
+        assert r.stderr == "ERROR: toxini file 'tox.ini' not found\n"
+        assert r.files_after == {}
+
+    def test_dtox_root_dir_param(self):
+
+        r = run("/root/dir", expect_error=True)
+
+        assert r.returncode != 0
+        assert r.stdout == "using CODE_DIR=/root/dir\n"
+        assert r.stderr == "ERROR: toxini file 'tox.ini' not found\n"
+        assert r.files_after == {}
